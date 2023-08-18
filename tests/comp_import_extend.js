936586ff4ec2df776a6b5e0ff953fec15e2b99d5
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-18 21:39:00 +0530

    Update comp_import_extend.js

--- tests/comp_import_extend.js
+++ tests/comp_import_extend.js
@@ -1,18 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var path = require("path");
-var tape = require("tape");
-var protobuf = require("../index");
+let path = require("path");
+let tape = require("tape");
+let protobuf = require("../index");
 // to extend Root
 require("../ext/descriptor");
 tape.test("extensions", function (test) {
     // load document with extended field imported multiple times
-    var root = protobuf.loadSync(path.resolve(__dirname, "data/test.proto"));
+    let root = protobuf.loadSync(path.resolve(__dirname, "data/test.proto"));
     root.resolveAll();
     // convert to Descriptor Set
-    var decodedDescriptorSet = root.toDescriptor("proto3");
+    let decodedDescriptorSet = root.toDescriptor("proto3");
     // load back from descriptor set
-    var root2 = protobuf.Root.fromDescriptor(decodedDescriptorSet);
+    let root2 = protobuf.Root.fromDescriptor(decodedDescriptorSet);
     test.pass("should parse and resolve without errors");
     test.end();
 });
