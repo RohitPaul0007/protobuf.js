d9b7845edbeb01ed613779c9bd53de87853fdb8b
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-18 21:32:29 +0530

    Update comp_optional.js

--- tests/comp_optional.js
+++ tests/comp_optional.js
@@ -1,8 +1,8 @@
-var tape = require("tape");
+let tape = require("tape");
 
-var protobuf = require("..");
+let protobuf = require("..");
 
-var proto = "syntax = \"proto3\";\
+let proto = "syntax = \"proto3\";\
 \
 message Message {\
     int32 regular_int32 = 1;\
@@ -14,15 +14,15 @@ message Message {\
 ";
 
 tape.test("proto3 optional", function(test) {
-    var root = protobuf.parse(proto).root;
+    let root = protobuf.parse(proto).root;
 
-    var Message = root.lookup("Message");
+    let Message = root.lookup("Message");
     test.equal(Message.fields.optionalInt32.optional, true);
     test.equal(Message.fields.optionalInt32.options.proto3_optional, true);
     test.equal(Message.oneofs._optionalInt32.name, '_optionalInt32');
     test.deepEqual(Message.oneofs._optionalInt32.oneof, ['optionalInt32']);
 
-    var m = Message.create({});
+    let m = Message.create({});
     test.strictEqual(m.regularInt32, 0);
     test.strictEqual(m.optionalInt32, null);
 
