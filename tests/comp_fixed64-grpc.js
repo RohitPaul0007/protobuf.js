119134c9218c68c4404db954e00ca01e603c6532
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-18 21:42:21 +0530

    Update comp_fixed64-grpc.js

--- tests/comp_fixed64-grpc.js
+++ tests/comp_fixed64-grpc.js
@@ -1,10 +1,10 @@
-var tape = require("tape");
+let tape = require("tape");
 
-var protobuf = require("..");
+let protobuf = require("..");
 
 tape.test("fixed64 (grpc)", function(test) {
 
-    var root = protobuf.Root.fromJSON({
+    let root = protobuf.Root.fromJSON({
         nested: {
             test: {
                 nested: {
@@ -21,16 +21,16 @@ tape.test("fixed64 (grpc)", function(test) {
         }
     });
 
-    var Test = root.lookup("test.Test");
+    let Test = root.lookup("test.Test");
 
-    var buffer = Test.encode({
+    let buffer = Test.encode({
         int_64: '314159265358979'
     }).finish();
 
     test.equal(buffer.length, 9, "should encode a total of 9 bytes");
     test.equal(buffer[0], 9, "should encode id 1, wireType 1");
 
-    var decoded = Test.decode(buffer);
+    let decoded = Test.decode(buffer);
     // decoded.int_64 is a Long here, so this implicitly calls Long#toString:
     test.ok(decoded.int_64 == '314159265358979', "should decode back the original value");
 
