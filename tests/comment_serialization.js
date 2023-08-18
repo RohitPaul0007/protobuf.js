df16dfb2ee73871d1b6124578897385631d77832
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-18 21:49:09 +0530

    Update comment_serialization.js

--- tests/comment_serialization.js
+++ tests/comment_serialization.js
@@ -1,6 +1,6 @@
-var tape = require("tape");
+let tape = require("tape");
 
-var protobuf = require("..");
+let protobuf = require("..");
 
 tape.test("by default, drop comments through de/serialization", function(test) {
     test.plan(16);
@@ -9,7 +9,7 @@ tape.test("by default, drop comments through de/serialization", function(test) {
             throw test.fail(err.message);
         }
 
-        var copy = protobuf.Root.fromJSON(root.toJSON());
+        let copy = protobuf.Root.fromJSON(root.toJSON());
         test.ok(root.lookup("TestMessage").comment);
         test.notOk(copy.lookup("TestMessage").comment);
         test.ok(root.lookup("TestMessage.testField").comment);
@@ -19,15 +19,15 @@ tape.test("by default, drop comments through de/serialization", function(test) {
         test.ok(root.lookup("TestMessage.testOneof").comment);
         test.notOk(copy.lookup("TestMessage.testOneof").comment);
 
-        var rootService = root.lookupService("TestService");
-        var copyService = copy.lookupService("TestService");
+        let rootService = root.lookupService("TestService");
+        let copyService = copy.lookupService("TestService");
         test.ok(rootService.comment);
         test.notOk(copyService.comment);
         test.ok(rootService.methods["testMethod"].comment);
         test.notOk(copyService.methods["testMethod"].comment);
 
-        var rootEnum = root.lookup("TestEnum");
-        var copyEnum = copy.lookup("TestEnum");
+        let rootEnum = root.lookup("TestEnum");
+        let copyEnum = copy.lookup("TestEnum");
         test.ok(rootEnum.comment);
         test.notOk(copyEnum.comment);
         test.ok(rootEnum.comments.VALUE);
@@ -44,20 +44,20 @@ tape.test("preserve comments through de/serialization if option set", function(t
             throw test.fail(err.message);
         }
 
-        var toJSONOptions = {keepComments: true};
-        var copy = protobuf.Root.fromJSON(root.toJSON(toJSONOptions));
+        let toJSONOptions = {keepComments: true};
+        let copy = protobuf.Root.fromJSON(root.toJSON(toJSONOptions));
         test.equal(root.lookup("TestMessage").comment, copy.lookup("TestMessage").comment);
         test.equal(root.lookup("TestMessage.testField").comment, copy.lookup("TestMessage.testField").comment);
         test.equal(root.lookup("TestMessage.testMap").comment, copy.lookup("TestMessage.testMap").comment);
         test.equal(root.lookup("TestMessage.testOneof").comment, copy.lookup("TestMessage.testOneof").comment);
 
-        var rootService = root.lookupService("TestService");
-        var copyService = copy.lookupService("TestService");
+        let rootService = root.lookupService("TestService");
+        let copyService = copy.lookupService("TestService");
         test.equal(rootService.comment, copyService.comment);
         test.equal(rootService.methods["testMethod"].comment, copyService.methods["testMethod"].comment);
 
-        var rootEnum = root.lookup("TestEnum");
-        var copyEnum = copy.lookup("TestEnum");
+        let rootEnum = root.lookup("TestEnum");
+        let copyEnum = copy.lookup("TestEnum");
         test.equal(rootEnum.comment, copyEnum.comment);
         test.equal(rootEnum.comments.VALUE, copyEnum.comments.VALUE);
 
