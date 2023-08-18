b6d1356fd088df4af40b73f9224d91e068d5b4fe
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-18 21:34:20 +0530

    Update comp_negative-int32.js

--- tests/comp_negative-int32.js
+++ tests/comp_negative-int32.js
@@ -1,15 +1,15 @@
-var tape = require("tape");
+let tape = require("tape");
 
-var protobuf  = require("..");
+let protobuf  = require("..");
 
 tape.test("negative int32 values", function(test) {
-    var writer = protobuf.Writer.create();
+    let writer = protobuf.Writer.create();
     writer.int32(-5615122);
-    var buf = writer.finish();
+    let buf = writer.finish();
 
     test.equal(buf.length, 10, "should encode to 10 bytes");
 
-    var reader = protobuf.Reader.create(buf);
+    let reader = protobuf.Reader.create(buf);
 
     test.equal(reader.int32(), -5615122, "should decode from 10 bytes");
 
