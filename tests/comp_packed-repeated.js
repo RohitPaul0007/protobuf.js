8ad7d5c9e9c5b92edb6bb553caef8129a24309d3
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-18 21:27:56 +0530

    Update comp_packed-repeated.js

--- tests/comp_packed-repeated.js
+++ tests/comp_packed-repeated.js
@@ -1,28 +1,28 @@
-var tape = require("tape");
+let tape = require("tape");
 
-var protobuf = require("..");
+let protobuf = require("..");
 
-var proto1 = "message Test {\
+let proto1 = "message Test {\
   repeated uint32 a = 1 [packed = true];\
 }";
 
-var proto2 = "message Test {\
+let proto2 = "message Test {\
   repeated uint32 a = 1 [packed = false];\
 }";
 
-var msg = {
+let msg = {
     a: [1,2,3]
 };
 
 tape.test("packed repeated values", function(test) {
-    var root1 = protobuf.parse(proto1).root,
+    let root1 = protobuf.parse(proto1).root,
         root2 = protobuf.parse(proto2).root;
-    var Test1 = root1.lookup("Test"),
+    let Test1 = root1.lookup("Test"),
         Test2 = root2.lookup("Test");
     
-    var dec1 = Test2.decode(Test1.encode(msg).finish());
+    let dec1 = Test2.decode(Test1.encode(msg).finish());
     test.same(dec1, msg, "should decode packed even if defined non-packed");
-    var dec2 = Test1.decode(Test2.encode(msg).finish());
+    let dec2 = Test1.decode(Test2.encode(msg).finish());
     test.same(dec2, msg, "should decode non-packed even if defined packed");
 
     test.end();
